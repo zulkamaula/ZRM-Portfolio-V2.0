c5dbf4aaaaa8adcd4e75afbfcd9c211e3894942f
Zulkariski Mauladi <74296102+zulkamaula@users.noreply.github.com>
2022-07-26 06:37:39 +0700

    Update typing.js

--- src/js/typing.js
+++ src/js/typing.js
@@ -10,6 +10,9 @@ typewriter
   .typeString('Zulkariski Mauladi')
   .pauseFor(3000)
   .deleteChars(13)
-  .typeString('Maula')
+  .typeString(' Maula')
   .pauseFor(1000)
+  .deleteChars(10)
+  .typeString('Born on July 09 1998')
+  .pauseFor(2500)
   .start();
