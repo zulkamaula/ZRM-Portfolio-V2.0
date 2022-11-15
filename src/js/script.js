07c62eaa2db1a2582708b2ea1d67c62c987447c5
zulkamaula <mzulkariski@gmail.com>
2022-11-15 13:43:35 +0700

    update info

--- src/js/script.js
+++ src/js/script.js
@@ -38,19 +38,19 @@ const loader = document.querySelector(".loader");
 loader.style.display = "none";
 
 contactForm.addEventListener("submit", function (e) {
-e.preventDefault();
-loader.style.display = "block";
-const url = e.target.action;
-const formData = new FormData(contactForm);
+    e.preventDefault();
+    loader.style.display = "block";
+    const url = e.target.action;
+    const formData = new FormData(contactForm);
 
-fetch(url, {
-    method: "POST",
-    body: formData,
-    mode: "no-cors",
-})
+    fetch(url, {
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+    })
     .then(() => {
-    loader.style.display = "none";
-    window.location.href = "/public/thankyou.html";
+        loader.style.display = "none";
+        window.location.href = "/public/thankyou.html";
     })
     .catch((e) => alert("Error occured"));
 });
